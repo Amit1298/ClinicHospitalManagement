99cb335ed60d339f4b95a020ab8d6d1dab62d46e
Amit <amitkr24031995@gmail.com>
2022-04-21 07:30:43 +0530

    Adding new address

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,8 @@ app.listen(4500,()=>{
 const express = require('express');
 const app = express();
 
-const Hospital = [
+app.use(express.json());
+const hospitals = [
     {
         DrID: "113",
         PasentName: "Amit"
@@ -43,13 +44,43 @@ const Hospital = [
     },
 ];
 
-app.get("/Hospital",(req,res)=>{
+app.get("/hospitals",(req,res)=>{
+    res.setHeader("Contant-Type","application/json");
+    res.setHeader("My-Header","ABC");
+    // res.status(500).json({message:"Something went wrong"});
     const data = JSON.stringify({
         success: true,
-        result: Hospital
+        result: hospitals
     });
     res.send(data);
 });
+
+app.get("/",(req,res)=>{
+    console.log(req);
+    console.log(req.query);
+
+    res.send('data');
+});
+
+// app.get("/api/hospitals",(req,res)=>{
+//     req.setHeader("Content-type","application/json");
+
+//     res.json({
+//         success:true,
+//         result: hospitals
+//     })
+// })
+
+app.get("/api/hospital",(req,res)=>{
+    console.log(req.query);
+    res.status(200).json({message: "Scussfully",data:req.query})
+})
+
+app.post('/api/create-book', (req, res)=> {
+    console.log(req.body);
+    res.status(200).json({message: 'Sucessful'});
+    res.status(200).json({message:"Successful",data:req.body});
+    });
 
 app.listen(4500,()=>{
     console.log("Server is Started");
